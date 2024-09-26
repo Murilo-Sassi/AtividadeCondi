b3d882e5c91659e62031c0f9610d05831b2a7d53
Murilo-Sassi <sassimurilo04@gmail.com>
2024-09-26 11:15:37 -0300

    AtividadeCondiçoes

---
+++ src/Atividade3.java
@@ -0,0 +1,15 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+
+        Integer numero = 19;
+
+        // codigo para par ou impar
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " É par");
+        }else {
+            System.out.println(numero + " É impar");
+        }
+
+    }
+}
